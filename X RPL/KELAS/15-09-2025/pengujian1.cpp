7e8cfb528785a519ecca0640fbaa02132d88b546
Dinda Dwi Wulandari <dindgp@gmail.com>
2025-09-26 18:36:04 +0700

    Add files via upload

---
+++ X RPL/KELAS/15-09-2025/pengujian1.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+using namespace std;
+int main() {
+  cout << "Belajar Program\n";
+  int angka;
+  cout << "Inputkan Angka: ";
+  cin >> angka;
+  cout << angka;
+  return 0;
+}
