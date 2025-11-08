f4a887fe69997da33985e53d9e9a6fee267a6420
Dinda Dwi Wulandari <dindgp@gmail.com>
2025-11-08 09:44:05 +0700

    Add files via upload

---
+++ X RPL/KELAS/23-10-2025/login.php
@@ -0,0 +1,21 @@
+<h1>login</h1>
+
+<form action="" method="POST">
+    <input type="email" name="email" placeholder="email" required> <br>
+    <input type="password" name="password" placeholder="password" required> <br>
+    <input type="submit" value="login" name="login">
+</form>
+
+<?php
+    if (isset($_POST['login'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if ($email == "admin@gmail.com" && $password == "12345"){
+            $_SESSION['email'] = $email;
+            header("location: index.php");
+        }else{
+            echo "email atau password salah";
+        }
+    } 
+?>
---
+++ X RPL/KELAS/23-10-2025/register.php
@@ -0,0 +1,8 @@
+<h1>Register</h1>
+
+<form action="get">
+    <input type="text" name="nama" placeholder="Nama lengkap"> <br>
+    <input type="email" name="email" placeholder="Email"> <br>
+    <input type="password" name="password" placeholder="Password"> <br>
+    <input type="submit" value="register" name="Register">
+</form>
