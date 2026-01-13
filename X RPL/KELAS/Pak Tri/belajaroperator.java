3d6121c06370e6c7ba71572f5c818f2810a81142
Dinda Dwi Wulandari <dindgp@gmail.com>
2026-01-13 14:07:36 +0700

    Add files via upload

---
+++ X RPL/KELAS/Pak Tri/belajaroperator.java
@@ -0,0 +1,69 @@
+public class belajaroperator {
+
+   
+    public static void main(String[] args) {
+        // Operator Aritmatika
+        int a= 10;
+        int b= 3;
+        
+        int penjumlahan= a+b;
+        int pengurangan= a-b;
+        int perkalian= a*b;
+        double pembagian= a/b;
+        int modulo= a%b;
+        
+            System.out.println("Hasil penjumlahan 2 angka adalah"+penjumlahan);
+            System.out.println("Hasil pengurangan 2 angka adalah"+pengurangan);
+            System.out.println("Hasil perkalian 2 angka adalah"+perkalian);
+            System.out.println("Hasil pembagian 2 angka adalah"+pembagian);
+            System.out.println ("Hasil modulo 2 angka adalah"+modulo);
+            
+        //Operator Penugasan
+        //=Memberi Nilai
+        //+=Penjaumlahan Nilai
+        //-=
+        //*=   
+        //%=
+        
+        int c=20;
+        
+        c +=5;
+        a-=2;
+        b *=100;
+        
+            System.out.println(c);
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println();
+            
+        //Operator Pembanding
+        
+        int d=50;
+        int e=10;
+        
+        boolean hasil= d==e;
+        boolean hasill= d>=e;
+        boolean hasil2= d!=e;
+        boolean hasil3= d<=e;
+        
+            System.out.println(hasil);
+            System.out.println(hasill);
+            System.out.println(hasil2);
+            System.out.println(hasil3);
+            
+        //Operator Logika
+        
+        boolean result = true && true;
+        boolean result2 = d>e && d==e;
+        boolean result3 = true||true;
+        boolean result4 = d!=e || d<e;
+        
+            System.out.println("Operator Logika-1"+result);
+            System.out.println("Operator Logika-2"+result2);
+            System.out.println("Operator Logika-3"+result3);
+            System.out.println("Operator Logika-4"+result4);
+            
+            
+    }
+    
+}
